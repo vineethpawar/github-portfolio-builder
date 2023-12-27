88b366182b9a72809e2ac2ee03727fb75a44c2df
vineethpawar <vineethpawar99@gmail.com>
2023-12-27 08:21:37 +0530

    chrore: added context store and wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
@@ -7,17 +7,41 @@ const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
+const myContext = React.createContext();
+
+const Provider = props => {
+  const [profile, setProfile] = useState({});
+  // get profile
+  useEffect(() => {
+    homeSectionService?.getProfile('vineethpawar')?.then(res => {
+      setProfile(res);
+    });
+  }, []);
+
+  return (
+    <myContext.Provider
+      value={{
+        profile,
+      }}
+    >
+      {props.children}
+    </myContext.Provider>
+  );
+};
+
 const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
+  <Provider>
+    <Layout location={location}>
+      <StyledMainContainer className="fillHeight">
+        <Hero />
+        <About />
+        <Jobs />
+        <Featured />
+        <Projects />
+        <Contact />
+      </StyledMainContainer>
+    </Layout>
+  </Provider>
 );
 
 IndexPage.propTypes = {
